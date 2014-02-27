2dd220ddd8d7d21cb2a5418df76d14171e8c429b
gubi <gubi.ale@iod.io>
2014-02-27 18:50:36 +0100

    Added readonly feature

--- bootstrap-fa-star-rating.js
+++ bootstrap-fa-star-rating.js
@@ -3,53 +3,60 @@
 $.fn.rating = function(params, callback) {
 	$.str_repeat = function(input, multiplier) { var y = ""; while (true) { if (multiplier & 1) { y += input; } multiplier >>= 1; if (multiplier) { input += input; } else { break; }} return y; };
 	var settings = $.extend({
-		rate: 0,
+		startRate: 0,
 		total: 0,
 		
 		stars: 5,
 		textVote: "rate",
-		textVotes: "rates"
+		textVotes: "rates",
+		readOnly: false,
+		readOnlyMessage: "If you want to rate please login",
+		readOnlyLink: "./Login"
 	}, params);
 	
-	var rat = "" + settings.rate,
+	var rat = "" + settings.startRate,
 	split_rate = rat.split("."),
-	box = $('<span class="text-star"></span>'),
+	box =  ((settings.readOnly) ? $('<a href="' + settings.readOnlyLink + '" class="text-star" title="' + settings.readOnlyMessage + '"></a>') : $('<span class="text-star"></span>')),
 	total_rates = $('<small id="total_rates"></small>'),
-	current_poll = $.str_repeat('<small class="fa fa-star"></small>', split_rate[0]) + ((split_rate[1] != null) ? '<small class="fa fa-star-half-o"></small>' : "") + $.str_repeat('<small class="fa fa-star-o"></small>', (settings.stars - Math.ceil(settings.rate))),
+	current_poll = $.str_repeat('<small class="fa fa-star"></small>', split_rate[0]) + ((split_rate[1] != null) ? '<small class="fa fa-star-half-o"></small>' : "") + $.str_repeat('<small class="fa fa-star-o"></small>', (settings.stars - Math.ceil(settings.startRate))),
 	empty_poll = $.str_repeat('<small class="fa fa-star-o"></small>', settings.stars);
 	
 	box.html(current_poll);
-	total_rates.html("(<small>" + settings.rate + "</small>/" + settings.total + " " + ((settings.total == 1) ? settings.textVote : settings.textVotes) + ")");
+	total_rates.html("(<small>" + settings.startRate + "</small>/" + settings.total + " " + ((settings.total == 1) ? settings.textVote : settings.textVotes) + ")");
 	
 	$(this).html(box);
 	$(this).append(" ");
 	$(this).append(total_rates);
 	
-	$(this).hover(function() {
-		$(".text-star").css({cursor: "default"});
-		
-		$(".text-star").hover(function() {
-			$(this).css({cursor: "default"});
+	if(!settings.readOnly) {
+		$(this).hover(function() {
+			$(".text-star").css({cursor: "default"});
 			
-			$(".text-star small").hover(function() {
-				$(this).css({cursor: "pointer"}).removeClass("fa-star-o").removeClass("fa-star-half-o").addClass("fa-star").css("font-size", "1.3em");
-				$(this).prevAll().each(function() {
-					$(this).removeClass("fa-star-o").removeClass("fa-star-half-o").addClass("fa-star").css("font-size", "1em");
+			$(".text-star").hover(function() {
+				$(this).css({cursor: "default"});
+				
+				$(".text-star small").hover(function() {
+					$(this).css({cursor: "pointer"}).removeClass("fa-star-o").removeClass("fa-star-half-o").addClass("fa-star").css("font-size", "1.3em");
+					$(this).prevAll().each(function() {
+						$(this).removeClass("fa-star-o").removeClass("fa-star-half-o").addClass("fa-star").css("font-size", "1em");
+					});
+					$(this).nextAll().each(function() {
+						$(this).removeClass("fa-star-half-o").removeClass("fa-star").addClass("fa-star-o").css("font-size", "1em");
+					});
+					$(this).click(function() {
+						var amount = $(this).index() + 1;
+						if (callback && typeof(callback) === "function") {
+							callback(amount);
+						}
+					});
 				});
-				$(this).nextAll().each(function() {
-					$(this).removeClass("fa-star-half-o").removeClass("fa-star").addClass("fa-star-o").css("font-size", "1em");
-				});
-				$(this).click(function() {
-					var amount = $(this).index() + 1;
-					if (callback && typeof(callback) === "function") {
-						callback(amount);
-					}
-				});
+			}, function() {
+				$(".text-star").html(current_poll);
 			});
 		}, function() {
-			$(".text-star").html(current_poll);
+			$(this).find(".text-star").html(current_poll);
 		});
-	}, function() {
-		$(this).find(".text-star").html(current_poll);
-	});
+	} else {
+		box.tooltip();
+	}
 }
